92aee57a050cd4f437aca133787542d5bd2d6b64
José Maria Marino Pereyra <mpjm.acc@gmail.com>
2022-02-12 04:50:06 -0300

    ejercicios de string e iteracion completados de distintas maneras

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -297,19 +297,55 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  
+
+  // 1) solucion utilizando if/else statements con cuerpo
+  // if(numero > 0) {
+  //   return "Es positivo"
+  // }
+  // else if (numero < 0 ) {
+  //   return "Es negativo"
+  // }
+  // else {
+  //   return false
+  // }
+
+  // 2) solucion utilizando if/else if/else statement sin cuerpo
+  // if(numero > 0) return "Es positivo"
+  // else if (numero < 0 ) return "Es negativo"
+  // else return false
+
+  // 3) solucion utilizando operador ternario encadenado
+  return numero > 0 ? "Es positivo" : numero < 0 ? "Es negativo" : false
 }
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+
+  // 1) solucion utilizando Objeto String y su metodo concat
+  // return str.concat("!")
+
+  // 2) solucion utilizando operador aritmetico de suma
+  // return str + "!"
+
+  // 3) solucion utilizando template strings
+  return `${str}!`
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
+
+  // 1) solucion utilizando Objeto String y su metodo concat
+  // return nombre.concat(" ", apellido)
+
+  // 2) solucion utilizando operador aritmetico de suma
+  // return nombre + " " + apellido
+
+  // 3) solucion utilizando template strings
+  return `${nombre} ${apellido}`
   
 }
 
@@ -317,27 +353,41 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
+
+  // solucion utilizando Objeto String y su metodo concat
+  // return "Hola ".concat(nombre,"!")
+
+  // solucion utilizando operador aritmetico de suma
+  // return "Hola " + nombre + "!"
   
+  // solucion utilizando template strings
+  return `Hola ${nombre}!`
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
   
+  // solucion utilizando operador aritmetico de multiplicacion
+  return alto * ancho
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
+
+  // solucion utilizando operador aritmetico de multiplicacion
+  return lado * 4
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-
+  
+  // solucion utilizando operador aritmetico de multiplicacion
+  return (base * altura) / 2
 }
 
 
@@ -345,7 +395,9 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+
+  // solucion utilizando operador aritmetico de multiplicacion
+  return euro * 1.20
 }
 
 
@@ -355,7 +407,55 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+
+  // solucion utilizando if/else statements con su bloque de codigo
+  // declaro una variable con una string que contenga las vocales
+  // const vocals = "aeiou"
+  // if (vocals.includes(letra)) {
+  //   return "Es vocal"
+  // }
+  // else if (letra > 1 || !vocals.includes(letra)) {
+  //   return "Dato incorrecto"
+  // }
+
+  // solucion utilizando if/else statements sin bloque de codigo
+  // const vocals = "aeiou"
+  // if (vocals.includes(letra)) return "Es vocal"
+  // else if (letra > 1 || !vocals.includes(letra)) return "Dato incorrecto"
+
+  // solucion utilizando operador ternario y operador logico
+  // const vocals = "aeiou"
+  // return vocals.includes(letra) ? "Es vocal" : ((letra > 1 || !vocals.includes(letra)) && "Dato incorrecto")
+
+  // solucion utilizando buble for
+  // const vocals = "aeiou"
+  // if (letra > 1) return "Dato incorrecto"
+  // for (let i = 0; i < vocals.length; i++) {
+  //   if (vocals[i] === letra) return "Es vocal"
+    
+  // }
+  // return "Dato incorrecto"
+
+  // solucion utilizando buble for of
+  const vocals = "aeiou"
+  if(letra > 1) return "Dato incorrecto"
+  for (const value of vocals) {
+    if (value === letra) return "Es vocal"
+  }
+  return "Dato incorrecto"
+
+
+  // solucion utilizando switch statement
+  // switch (letra) {
+  //   case "a":
+  //   case "e":
+  //   case "i":
+  //   case "o":
+  //   case "u":
+  //     return "Es vocal"
+  //   default: 
+  //     return "Dato incorrecto"
+  // }
 }
 
 
