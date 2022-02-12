783fe1cd781b4e3dbd4b098259e2f521c9b540ea
José Maria Marino Pereyra <mpjm.acc@gmail.com>
2022-02-11 22:00:44 -0300

    operaciones matematicas basicas resueltas

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,6 +28,7 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
+  return str
   
 }
 
@@ -35,25 +36,25 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  
+  return x + y
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  
+  return x - y
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-  
+  return x * y
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  
+  return x / y
 }
 
 function sonIguales(x, y) {
